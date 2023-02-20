a95cddc75c17795b7d4ac104e004963b66d7451d
HuriatiPutri <Huriatiputri@gmail.com>
2023-02-20 21:05:31 +0700

    Update vite.config.ts
    chunk

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,16 @@ export default defineConfig({
       "@mui/lab": "@material-ui/lab",
       epubjs: "epubjs/dist/epub.js"
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+}
 });
